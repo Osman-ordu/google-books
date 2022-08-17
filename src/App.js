27c30555c47e54cdda4acbf7adb9303d80b225a8
Osman-ordu <92692879+Osman-ordu@users.noreply.github.com>
2022-08-17 19:40:45 +0300

    --

--- src/App.js
+++ src/App.js
@@ -1,53 +1,148 @@
-import React,{useState} from "react";
-import './app.css'
-import { 
+import React, { useState } from 'react';
+import './App.css';
+import {
   InputGroup,
   Input,
   Button,
   FormGroup,
-  Label  
-} from 'reactstrap'
-
-const App = () => {
-  // state
-  const [maxResult,setMaxResults] = useState(10);
-  const [startIndex,setStartIndex] = useState(1);
+  Label,
+  Spinner
+} from 'reactstrap';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.min.css';
+import axios from 'axios';
+import BookCard from './BookCard.jsx';
+function App() {
+  // States
+  const [maxResults, setMaxResults] = useState(10);
+  const [startIndex, setStartIndex] = useState(1);
+  const [query, setQuery] = useState('');
+  const [loading, setLoading] = useState(false);
+  const [cards, setCards] = useState([]);
+  // Handle Search
+  const handleSubmit = () => {
+    setLoading(true);
+    if (maxResults > 40 || maxResults < 1) {
+      toast.error('max results must be between 1 and 40');
+    } else {
+      axios
+        .get(
+          `https://www.googleapis.com/books/v1/volumes?q=${query}&maxResults=${maxResults}&startIndex=${startIndex}`
+        )
+        .then(res => {
+          if (startIndex >= res.data.totalItems || startIndex < 1) {
+            toast.error(
+              `max reults must be between 1 and ${res.data.totalItems}`
+            );
+          } else {
+            if (res.data.items.length > 0) {
+              setCards(res.data.items);
+              setLoading(false);
+            }
+          }
+        })
+        .catch(err => {
+          setLoading(true);
+          console.log(err.response);
+        });
+    }
+  };
+  // Main Show Case
   const mainHeader = () => {
-
     return (
-      <div className="main-image d-flex justify-content-center align-items-center flex-column" > 
-      {/* overlay */}
-      <div className="filter">
-        <h1 className="display-2 text-center text-white mb-3 fw-bold" style={{position: 'relative',zIndex: '2'}}>
+      <div className='main-image d-flex justify-content-center align-items-center flex-column'>
+        {/* Overlay */}
+        <div className='filter'></div>
+        <h1
+          className='display-2 text-center text-white mb-3 fw-bold'
+          style={{ zIndex: 2 }}
+        >
           Google Books
-        </h1>   
-      </div>
-      <div style={{width: '60%',zIndex: '2'}}>
-          <InputGroup size="lg" className="mb-3 ">
-            <Input  className="shadow-none border-0" placeholder="Books Search" />
-            <Button className="color-secondary">
-            <i className="fas fa-search"></i>
-          </Button>
+        </h1>
+        <div style={{ width: '60%', zIndex: 2 }}>
+          <InputGroup size='lg' className='mb-3'>
+            <Input
+              placeholder='Book Search..'
+              className='shadow-none'
+              value={query}
+              onChange={e => setQuery(e.target.value)}
+              
+            />
+              <Button color='secondary' onClick={handleSubmit}>
+                <i className='fas fa-search'></i>
+              </Button>
           </InputGroup>
-          <div className="d-flex text-white justify-content-center">
-            <FormGroup className="mx-2 fs-6 fw-bold">Max Results
-              <Label for='maxResults'></Label>
-              <Input className="shadow-none border-0" type="number" id="maxResults" placeholder="Max Results"/>
+          <div className='d-flex text-white justify-content-center'>
+            <FormGroup >
+              <Label for='maxResults'>Max Results</Label>
+              <Input
+                type='number'
+                id='maxResults'
+                placeholder='Max Results'
+                className='shadow-none'
+                value={maxResults}
+                onChange={e => setMaxResults(e.target.value)}
+              />
             </FormGroup>
-            <FormGroup className="mx-2 fs-6 fw-bold">Start Index
-              <Label for='startIndex'></Label>
-              <Input className="shadow-none border-0" type="number" id="startIndex" placeholder="Start Index"/>
+            <FormGroup className='mx-3'>
+              <Label for='startIndex'>Start Index</Label>
+              <Input
+                type='number'
+                id='startIndex'
+                className='shadow-none'
+                placeholder='Start Index'
+                value={startIndex}
+                onChange={e => setStartIndex(e.target.value)}
+              />
             </FormGroup>
           </div>
-    
-          
         </div>
       </div>
-    )
-  }
+    );
+  };
+
+  const handleCards = () => {
+    if (loading) {
+      return (
+        <div className='d-flex justify-content-center mt-3'>
+          <Spinner style={{ width: '4rem', height: '4rem' }} />
+        </div>
+      );
+    } else {
+      const items = cards.map((item, i) => {
+        let thumbnail = '';
+        if (item.volumeInfo.imageLinks) {
+          thumbnail = item.volumeInfo.imageLinks.thumbnail;
+        }
+
+        return (
+          <div className='col-lg-4 mb-3' key={item.id}>
+            <BookCard
+              thumbnail={thumbnail}
+              title={item.volumeInfo.title}
+              pageCount={item.volumeInfo.pageCount}
+              language={item.volumeInfo.language}
+              authors={item.volumeInfo.authors}
+              publisher={item.volumeInfo.publisher}
+              description={item.volumeInfo.description}
+              previewLink={item.volumeInfo.previewLink}
+              infoLink={item.volumeInfo.infoLink}
+            />
+          </div>
+        );
+      });
+      return (
+        <div className='container my-5'>
+          <div className='row'>{items}</div>
+        </div>
+      );
+    }
+  };
   return (
-    <div className="App">
+    <div className='w-100 h-100'>
       {mainHeader()}
+      {handleCards()}
+      <ToastContainer />
     </div>
   );
 }
