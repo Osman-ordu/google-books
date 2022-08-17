71b9eba0b53d731e9d9e6860e607e71e8b6a4f97
Osman-ordu <92692879+Osman-ordu@users.noreply.github.com>
2022-08-17 17:10:06 +0300

    google-book first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,52 @@
-import logo from './logo.svg';
-import './App.css';
+import React,{useState} from "react";
+import './app.css'
+import { 
+  InputGroup,
+  Input,
+  Button,
+  FormGroup,
+  Label  
+} from 'reactstrap'
 
-function App() {
+const App = () => {
+  // state
+  const [maxResult,setMaxResults] = useState();
+  const mainHeader = () => {
+
+    return (
+      <div className="main-image d-flex justify-content-center align-items-center flex-column" > 
+      {/* overlay */}
+      <div className="filter">
+        <h1 className="display-2 text-center text-white mb-3 fw-bold" style={{position: 'relative',zIndex: '2'}}>
+          Google Books
+        </h1>   
+      </div>
+      <div style={{width: '60%',zIndex: '2'}}>
+          <InputGroup size="lg" className="mb-3 ">
+            <Input  className="shadow-none border-0" placeholder="Books Search" />
+            <Button className="color-secondary">
+            <i className="fas fa-search"></i>
+          </Button>
+          </InputGroup>
+          <div className="d-flex text-white justify-content-center">
+            <FormGroup className="mx-2 fs-6 fw-bold">Max Results
+              <Label for='maxResults'></Label>
+              <Input className="shadow-none border-0" type="number" id="maxResults" placeholder="Max Results"/>
+            </FormGroup>
+            <FormGroup className="mx-2 fs-6 fw-bold">Start Index
+              <Label for='startIndex'></Label>
+              <Input className="shadow-none border-0" type="number" id="startIndex" placeholder="Start Index"/>
+            </FormGroup>
+          </div>
+    
+          
+        </div>
+      </div>
+    )
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {mainHeader()}
     </div>
   );
 }
